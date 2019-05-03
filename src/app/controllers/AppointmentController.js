04338c6b89f700381e278b5883585dd52a2084b3
Gabriel Costa <gabrielszcosta@gmail.com>
2019-05-03 00:04:52 -0400

    Criacao de rots e controller de agendamento

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,10 @@
+const { User, Appointment } = require('../models')
+
+class AppointmentController {
+  async create (req, res) {
+    const provider = await User.findByPk(req.params.provider)
+    return res.render('appointments/create', { provider })
+  }
+}
+
+module.exports = new AppointmentController()
